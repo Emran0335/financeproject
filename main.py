eea8e00ad25daa2df271c21eec6794f244fe7e38
NusaibaJahan <nusaibajahansadifa@gmail.com>
2024-11-30 23:45:05 +0600

    how to fetch code from remote repository to local working directory

--- main.py
+++ main.py
@@ -3,3 +3,5 @@
 # Though I am on the main branch, but feature3 will not created 
 # based on main, rather it will all code from feature2
 # as we have explicitly mention feature2
+# git pull origin main
+# -> it will fetch all codes from the origin main branch to the local working directory.
