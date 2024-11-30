518179fc4cfc2aa8252a6dc5169450e8eaa36ea9
NusaibaJahan <nusaibajahansadifa@gmail.com>
2024-11-30 13:06:43 +0600

    git checkout cli is explained

--- main.py
+++ main.py
@@ -6,3 +6,6 @@
 # git branch feature1 -> to create branch;
 # git checkout feature1 -> to switch to feature1
 # created branch will copy the code of branch from where we are creating the branch;
+# git checkout -b feature1
+# this command will not work as feature1 is already created
+# so, git checkout feature1 is enough to switch to the branch
