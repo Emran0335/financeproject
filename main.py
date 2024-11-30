48aba3f0e356c46b669d3abe6a10554c8e664c51
NusaibaJahan <nusaibajahansadifa@gmail.com>
2024-11-30 12:30:34 +0600

    feature1 branch is created from main branch and main file is modified

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 # This is the main python main file.
+
+# This is from feature1 branch;
+
+# git branch -> to check branch;
+# git branch feature1 -> to create branch;
+# git checkout feature1 -> to switch to feature1
+# created branch will copy the code of branch from where we are creating the branch;
