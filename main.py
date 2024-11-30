7d250beb051902741634621c147d0935e56f7cc4
NusaibaJahan <nusaibajahansadifa@gmail.com>
2024-11-30 12:51:15 +0600

    feature2 created based on feature1 and modified using git cli

--- main.py
+++ main.py
@@ -6,3 +6,6 @@
 # git branch feature1 -> to create branch;
 # git checkout feature1 -> to switch to feature1
 # created branch will copy the code of branch from where we are creating the branch;
+
+# git checkout -b feature2
+# it will checkout feature2 and create featue2 if it is not found.
