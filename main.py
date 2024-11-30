3f0cd2302d3736d9eca1d61d45029ad417a81d16
NusaibaJahan <nusaibajahansadifa@gmail.com>
2024-11-30 13:16:41 +0600

    git checkout cli is explained in more details

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 # This is the main python main file.
+# git checkout -b feature3 feature2
+# Though I am on the main branch, but feature3 will not created 
+# based on main, rather it will all code from feature2
+# as we have explicitly mention feature2
