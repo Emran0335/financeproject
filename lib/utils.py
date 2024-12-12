c01450c585d435c366fd563d1423dd0b4c0ed3c2
emran0335 <emranhossain0335@gmail.com>
2024-12-12 22:59:19 +0600

    made changes to the utils of feature1 branch

--- lib/utils.py
+++ lib/utils.py
@@ -1,2 +1,2 @@
 #This is a file that contains utility functions such as creating spark session
-# cat README.md to reade file in the folder in the terminal
+# cat README.md - to read README.md file of a folder in the terminal.
